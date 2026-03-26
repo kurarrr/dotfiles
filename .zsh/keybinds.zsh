35f4be4eef625dc0cf1c8bf303f83312d09c9506
kurarrr <reverse.engau@gmail.com>
2026-03-26 12:23:20 +0900

    Add keybindings for word navigation in keybinds.zsh

--- .zsh/keybinds.zsh
+++ .zsh/keybinds.zsh
@@ -1,3 +1,9 @@
+# Option+Left/Right: word navigation (prevent entering vi normal mode)
+bindkey '\e[1;3D' backward-word   # iTerm2: Option+Left
+bindkey '\e[1;3C' forward-word    # iTerm2: Option+Right
+bindkey '\eb'     backward-word   # Terminal.app: Option+Left
+bindkey '\ef'     forward-word    # Terminal.app: Option+Right
+
 fzf-z-search() {
   local res=$(zoxide query -l | fzf --no-sort)
   if [ -n "$res" ]; then
